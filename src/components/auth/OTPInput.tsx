19881726e0e79142a6ae3e69b86df977d1dbeae7
Syncsphere7 <syncsphere7@gmail.com>
2025-11-03 21:10:41 +0300

    Fix: OTPInput ref callback must return void

--- src/components/auth/OTPInput.tsx
+++ src/components/auth/OTPInput.tsx
@@ -95,7 +95,9 @@ export default function OTPInput({ length = 6, onComplete, disabled = false }: O
       {otp.map((digit, index) => (
         <input
           key={index}
-          ref={(el) => (inputRefs.current[index] = el)}
+          ref={(el) => {
+            inputRefs.current[index] = el
+          }}
           type="text"
           inputMode="numeric"
           maxLength={1}
